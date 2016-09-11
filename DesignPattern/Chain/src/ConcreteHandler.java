0fc84af1376f07fe09ccff48ed5520e1a066a98d
ValarHao <610652933@qq.com>
2016-09-11 16:51:28 +0800

    First commit

---
+++ DesignPattern/Chain/src/ConcreteHandler.java
@@ -0,0 +1,20 @@
+/**
+ * 具体处理者角色：具体处理者接到请求后，可以选择将请求处理掉，或者将请求传给下家。
+ * 由于具体处理者持有对下家的引用，因此，如果需要，具体处理者可以访问下家。
+ */
+public class ConcreteHandler extends Handler {
+
+    /**
+     * 处理方法，调用此方法处理请求
+     */
+    @Override
+    public void handleRequest() {
+
+        if (getSuccessor() != null) {
+            System.out.println("放过请求");
+            getSuccessor().handleRequest();
+        } else {
+            System.out.println("处理请求");
+        }
+    }
+}
---
+++ DesignPattern/Chain/src/Handler.java
@@ -0,0 +1,34 @@
+/**
+ * 抽象处理者角色：定义出一个处理请求的接口。
+ * 如果需要，接口可以定义 出一个方法以设定和返回对下家的引用。
+ * 这个角色通常由一个Java抽象类或者Java接口实现。
+ */
+public abstract class Handler {
+
+    /**
+     * 持有后继的责任对象
+     */
+    protected Handler successor;
+
+    /**
+     * 示意处理请求的方法，虽然这个示意方法是没有传入参数的
+     * 但实际是可以传入参数的，根据具体需要来选择是否传递参数
+     */
+    public abstract void handleRequest();
+
+    /**
+     * 取值方法
+     * @return
+     */
+    public Handler getSuccessor() {
+        return successor;
+    }
+
+    /**
+     * 赋值方法，设置后继的责任对象
+     * @param successor
+     */
+    public void setSuccessor(Handler successor) {
+        this.successor = successor;
+    }
+}

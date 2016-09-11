64e28d2f2dcae7b4ec24358ed7b8a9875dbfe915
ValarHao <610652933@qq.com>
2016-09-11 20:13:39 +0800

    First commit

---
+++ DesignPattern/Prototype/src/ConcretePrototype.java
@@ -0,0 +1,6 @@
+public class ConcretePrototype extends Prototype {
+
+    public void show() {
+        System.out.println("原型模式实现类");
+    }
+}
---
+++ DesignPattern/Prototype/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ConcretePrototype cp = new ConcretePrototype();
+
+        for (int i=0; i<10; i++) {
+            ConcretePrototype clonecp = (ConcretePrototype) cp.clone();
+            clonecp.show();
+        }
+    }
+}
